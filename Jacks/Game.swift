32082f6e764bc51bf18bb0f84ef4aa7c5daf0057
Steve Gravrock <sdg@panix.com>
2014-12-27 13:29:32 -0800

    Break ties

--- Jacks/Game.swift
+++ Jacks/Game.swift
@@ -79,20 +79,84 @@ class Game: NSObject, ComputerPlayerDelegate {
 	
 	func winner() -> Player {
 		// precondition: game is finished
-		// TODO: Handle ties on points. Right now we choose an arbitrary winner in that case.
-		var result: Player? = nil
+		var candidates : [Player] = []
 		var minScore = Int.max
 		
 		for p in players {
 			let ps = p.score()
 			
 			if ps < minScore {
-				result = p
+				candidates = [p]
 				minScore = ps
+			} else if ps == minScore {
+				candidates.append(p)
 			}
 		}
 		
-		return result!
+		if candidates.count == 1 {
+			return candidates[0]
+		} else {
+			return breakTie(candidates)
+		}
+	}
+	
+	func breakTie(candidates: [Player]) -> Player {
+		if let winner = breakTieByJackCount(candidates) {
+			return winner
+		}
+		
+		return breakTieByHighestJack(candidates)
+	}
+
+	func breakTieByJackCount(candidates: [Player]) -> Player? {
+		var winners = [Player]()
+		var winnerJackCount = -1
+
+		for p in candidates {
+			let jacks = p.hand.filter({$0.value == CardValue.Jack})
+
+			if jacks.count > winnerJackCount {
+				winners = [p]
+				winnerJackCount = jacks.count
+			} else if jacks.count == winnerJackCount {
+				winners.append(p)
+			}
+		}
+
+		if winners.count == 1 {
+			return winners[0]
+		} else {
+			return nil
+		}
+	}
+	
+	func breakTieByHighestJack(candidates: [Player]) -> Player {
+		var suitsByRank = [Suit.Diamonds, Suit.Clubs, Suit.Hearts, Suit.Spades]
+		var winner: Player? = nil
+		var winnerScore = -1
+		
+		for p in candidates {
+			var score = -1
+			for c in p.hand {
+				if c.value == CardValue.Jack {
+					score = max(score, find(suitsByRank, c.suit)!)
+				}
+			}
+			
+			if score > winnerScore {
+				winner = p
+				winnerScore = score
+			}
+		}
+		
+		if let w = winner {
+			return w
+		} else {
+			// None of the candidates had any jacks.
+			// This is *exteremly* unlikely, but theoretically possible.
+			// Pick a winner arbitrarily.
+			return candidates[0]
+		}
 	}
 	
 	func computerPlayer(computerPlayer: Player, didMove move: Move) {
--- JacksTests/GameTests.swift
+++ JacksTests/GameTests.swift
@@ -13,4 +13,41 @@ class GameTests: XCTestCase {
 		target.players[2].hand[0] = Card(suit: Suit.Clubs, value: CardValue.Jack)
 		XCTAssertTrue(target.winner() === target.players[2], "Wrong winner")
     }
+	
+	func testPlayerWithMostJacksWinsTies() {
+		let target = Game()
+		for i in 0...3 {
+			let p = target.players[i]
+			for i in 0...3 {
+				p.hand[i] = Card(suit: Suit.Clubs, value: CardValue.Ten)
+			}
+		}
+		
+		target.players[0].hand[0] = Card(suit: Suit.Diamonds, value: CardValue.Jack)
+		target.players[0].hand[1] = Card(suit: Suit.Clubs, value: CardValue.Ace)
+		target.players[0].hand[2] = Card(suit: Suit.Clubs, value: CardValue.Nine)
+		target.players[1].hand[0] = Card(suit: Suit.Clubs, value: CardValue.Jack)
+		target.players[1].hand[1] = Card(suit: Suit.Clubs, value: CardValue.Jack)
+		XCTAssertEqual(target.players[0].score(), target.players[1].score(), "Scores should be equal")
+		XCTAssertTrue(target.winner() == target.players[1], "Wrong winner")
+	}
+	
+	
+	func testPlayerWithHighestJackWinsFurtherTies() {
+		let target = Game()
+		for p in target.players {
+			for j in 0...3 {
+				p.hand[j] = Card(suit: Suit.Clubs, value: CardValue.Ten)
+			}
+		}
+		
+		// Give players 1 and 2 each a pair of jacks. "High" jack according to
+		// D,C,H,S order should win.
+		target.players[0].hand[0] = Card(suit: Suit.Hearts, value: CardValue.Jack)
+		target.players[0].hand[1] = Card(suit: Suit.Clubs, value: CardValue.Jack)
+		target.players[1].hand[0] = Card(suit: Suit.Diamonds, value: CardValue.Jack)
+		target.players[1].hand[1] = Card(suit: Suit.Spades, value: CardValue.Jack)
+		XCTAssertTrue(target.winner() == target.players[1], "Wrong winner")
+	}
+
 }
